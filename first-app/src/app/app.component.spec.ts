cfb0cb5e7d1770ddd4231ed3dc4cb8dcd66d267a
panjie <3792535@qq.com>
2021-01-31 13:49:51 +0800

    2.2.3

--- first-app/src/app/app.component.spec.ts
+++ first-app/src/app/app.component.spec.ts
@@ -1,5 +1,4 @@
 import {TestBed} from '@angular/core/testing';
-import {RouterTestingModule} from '@angular/router/testing';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 
@@ -7,7 +6,6 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
         HttpClientModule
       ],
       declarations: [
