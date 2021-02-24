48e14fcc84f7406ca63e3db6cea141c1bd8152c7
teacherpan <3792535@qq.com>
2021-02-24 15:07:31 +0800

    init

---
+++ angualrguide/src/main/java/club/yunzhi/api/angualrguide/AngualrguideApplication.java
@@ -0,0 +1,13 @@
+package club.yunzhi.api.angualrguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngualrguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngualrguideApplication.class, args);
+	}
+
+}
---
+++ angualrguide/src/test/java/club/yunzhi/api/angualrguide/AngualrguideApplicationTests.java
@@ -0,0 +1,13 @@
+package club.yunzhi.api.angualrguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngualrguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
