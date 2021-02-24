4ff6ec0723ca655fca18a57a099e10ed180c8724
teacherpan <3792535@qq.com>
2021-02-24 21:45:10 +0800

    formart

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-import { AddComponent } from './add/add.component';
+import {AddComponent} from './add/add.component';
+
 
 @NgModule({
   declarations: [
@@ -19,4 +20,5 @@ import { AddComponent } from './add/add.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
