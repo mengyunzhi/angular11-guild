c8684b5dc74995c63d74409368dce0892a21b3d6
teacherpan <3792535@qq.com>
2021-02-24 20:53:08 +0800

    step2.3.1

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { AddComponent } from './add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
