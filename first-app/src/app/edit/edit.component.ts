4dbc27e7a3b1332cf05fd1cab368849b71d1c010
teacherpan <3792535@qq.com>
2021-02-27 14:08:18 +0800

    step2.4.5

--- first-app/src/app/edit/edit.component.ts
+++ first-app/src/app/edit/edit.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
+import {AppComponent} from '../app.component';
 
 @Component({
   selector: 'app-edit',
@@ -17,7 +18,8 @@ export class EditComponent implements OnInit {
   };
 
   constructor(private activeRoute: ActivatedRoute,
-              private httpClient: HttpClient) {
+              private httpClient: HttpClient,
+              private appComponent: AppComponent) {
   }
 
   ngOnInit(): void {
@@ -27,12 +29,23 @@ export class EditComponent implements OnInit {
     const url = 'http://angular.api.codedemo.club:81/teacher/' + id;
     // 发起请求，成功时并打印请求结果，失败时打印失败结果
     this.httpClient.get<{ name: string, username: string, email: string, sex: boolean }>(url)
-      .subscribe(data =>  this.teacher = data,
+      .subscribe(data => this.teacher = data,
         error => console.log('失败', error)
       );
   }
 
   onSubmit(): void {
-    console.log('点击提交按钮');
+    console.log(this.teacher);
+    // 获取ID，拼接URL
+    const url = 'http://angular.api.codedemo.club:81/teacher/' +
+      this.activeRoute.snapshot.params.id;
+    // 发起请求，更新教师，成功时打印请求结果并刷新教师列表查看效果，失败时打印失败结果
+    this.httpClient.put(url, this.teacher)
+      .subscribe(data => {
+          console.log('更新成功', data);
+          // 在此调用教师列表App组个的ngOnInit方法，即可实现更新后重新刷新教师列表的功能
+          this.appComponent.ngOnInit();
+        },
+        error => console.log('更新错误', error));
   }
 }
