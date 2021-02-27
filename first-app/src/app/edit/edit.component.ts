aa06f320296e64b78c0ad3ec46c39099bf3c60c1
teacherpan <3792535@qq.com>
2021-02-27 10:20:14 +0800

    2.4.3

--- first-app/src/app/edit/edit.component.ts
+++ first-app/src/app/edit/edit.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-edit',
@@ -7,13 +8,21 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
-
-  constructor(private activeRoute: ActivatedRoute) {
+  teacher: any;
+  constructor(private activeRoute: ActivatedRoute,
+              private httpClient: HttpClient) {
   }
 
   ngOnInit(): void {
     const id = this.activeRoute.snapshot.params.id;
-    console.log('获取到的路由参数id值为', id);
+
+    // 拼接请求URL
+    const url = 'http://angular.api.codedemo.club:81/teacher/' + id;
+    // 发起请求，成功时并打印请求结果，失败时打印失败结果
+    this.httpClient.get(url)
+      .subscribe(data => this.teacher = data,
+        error => console.log('失败', error)
+      );
   }
 
 }
