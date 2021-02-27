26a5fd2fabce1abbd43e81a1e3d21f847c2824bf
teacherpan <3792535@qq.com>
2021-02-27 13:07:04 +0800

    2.4.4

--- first-app/src/app/edit/edit.component.ts
+++ first-app/src/app/edit/edit.component.ts
@@ -8,7 +8,14 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
-  teacher: any;
+  // 使用as进行类型转换
+  teacher = {} as {
+    name: string,
+    username: string,
+    email: string,
+    sex: boolean
+  };
+
   constructor(private activeRoute: ActivatedRoute,
               private httpClient: HttpClient) {
   }
@@ -19,10 +26,13 @@ export class EditComponent implements OnInit {
     // 拼接请求URL
     const url = 'http://angular.api.codedemo.club:81/teacher/' + id;
     // 发起请求，成功时并打印请求结果，失败时打印失败结果
-    this.httpClient.get(url)
-      .subscribe(data => this.teacher = data,
+    this.httpClient.get<{ name: string, username: string, email: string, sex: boolean }>(url)
+      .subscribe(data =>  this.teacher = data,
         error => console.log('失败', error)
       );
   }
 
+  onSubmit(): void {
+    console.log('点击提交按钮');
+  }
 }
