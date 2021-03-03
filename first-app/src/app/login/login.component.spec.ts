66238650e6dc6a926a4d2429aba13a98e696ef14
teacherpan <3792535@qq.com>
2021-03-03 09:26:18 +0800

    3.1

--- first-app/src/app/login/login.component.spec.ts
+++ first-app/src/app/login/login.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import {LoginComponent} from './login.component';
+import {FormsModule} from '@angular/forms';
+import {root} from 'rxjs/internal-compatibility';
 
 fdescribe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,9 +10,12 @@ fdescribe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [LoginComponent],
+      imports: [
+        FormsModule
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +26,18 @@ fdescribe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    fixture.autoDetectChanges();
+
+    console.log(fixture.elementRef.nativeElement);
+    const rootDivElement = fixture.elementRef.nativeElement as HTMLDivElement;
+    const submitButtonElement = rootDivElement.querySelector('button') as HTMLButtonElement;
+    console.log(submitButtonElement);
+
+    spyOn(component, 'onSubmit').and.callFake(() => console.log('间谍方法被调用'));
+    // 点击按钮以前，onSubmit方法应该被调用了0次。
+    expect(component.onSubmit).toHaveBeenCalledTimes(0);
+    submitButtonElement.click();
+    // 点击按钮以后，onSubmit方法应该被调用了1次。
+    expect(component.onSubmit).toHaveBeenCalledTimes(1);
   });
 });
--- first-app/src/app/login/login.component.ts
+++ first-app/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  teacher = {} as {
+    username: string,
+    password: string
+  };
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    console.log('点击了登录按钮');
+  }
 }
