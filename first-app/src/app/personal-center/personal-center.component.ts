4f4eb68e279287e34ee19ba970ce9a73b82e7c84
teacherpan <3792535@qq.com>
2021-03-08 16:26:11 +0800

    login

--- first-app/src/app/personal-center/personal-center.component.ts
+++ first-app/src/app/personal-center/personal-center.component.ts
@@ -14,16 +14,16 @@ export class PersonalCenterComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const authString = 'zhangsan:codedemo.club';
-    const authToken = btoa(authString);
     let httpHeaders = new HttpHeaders();
-    httpHeaders = httpHeaders.append('Authorization', 'Basic ' + authToken);
+    httpHeaders = httpHeaders.append('x-auth-token', '02d4bb26-991e-4902-b80f-3ef436909766');
 
     const url = 'http://angular.api.codedemo.club:81/teacher/me';
-    this.httpClient.get<Teacher>(url, {headers: httpHeaders})
-      .subscribe(teacher => {
+    this.httpClient.get<Teacher>(url,
+      {headers: httpHeaders, observe: 'response'})
+      .subscribe(teacherResponse => {
           console.log('请求当前登录用户成功');
-          this.me = teacher;
+          this.me = teacherResponse.body as Teacher;
+          console.log(teacherResponse.headers.keys());
         },
         error => console.log('请求当前登录用户发生错误', error));
   }
