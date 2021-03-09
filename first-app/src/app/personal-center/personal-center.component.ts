3d0ae9fbbf47d90bc8240af42f9b1f0d174fcea6
teacherpan <3792535@qq.com>
2021-03-09 10:26:17 +0800

    4.4

--- first-app/src/app/personal-center/personal-center.component.ts
+++ first-app/src/app/personal-center/personal-center.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Teacher} from '../entity/teacher';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-personal-center',
@@ -15,15 +15,16 @@ export class PersonalCenterComponent implements OnInit {
 
   ngOnInit(): void {
     let httpHeaders = new HttpHeaders();
-    httpHeaders = httpHeaders.append('x-auth-token', '02d4bb26-991e-4902-b80f-3ef436909766');
-
+    const authString = 'zhangsan:codedemo.club';
+    const authToken = btoa(authString);
+    httpHeaders = httpHeaders.append('Authorization', 'Basic ' + authToken);
     const url = 'http://angular.api.codedemo.club:81/teacher/me';
     this.httpClient.get<Teacher>(url,
       {headers: httpHeaders, observe: 'response'})
-      .subscribe(teacherResponse => {
+      .subscribe((teacherResponse: HttpResponse<Teacher>) => {
           console.log('请求当前登录用户成功');
           this.me = teacherResponse.body as Teacher;
-          console.log(teacherResponse.headers.keys());
+          console.log(teacherResponse.headers.get('x-auth-token'));
         },
         error => console.log('请求当前登录用户发生错误', error));
   }
