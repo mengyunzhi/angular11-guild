e998491e92bc88f93e1b82cbfed421fea9690e56
teacherpan <3792535@qq.com>
2021-03-18 15:55:36 +0800

    增加测试示例

--- first-app/src/app/clazz/add/add.component.spec.ts
+++ first-app/src/app/clazz/add/add.component.spec.ts
@@ -13,6 +13,7 @@ describe('AddComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [AddComponent],
       imports: [FormsModule, HttpClientModule],
+      // 加入自定义的XAuthTokenInterceptor，让其自动为我们处理认证的header
       providers: [
         {provide: HTTP_INTERCEPTORS, multi: true, useClass: XAuthTokenInterceptor}
       ]
@@ -32,18 +33,25 @@ describe('AddComponent', () => {
   });
 
   fit('onSubmit', () => {
-    component.clazz = {
-      name: 'test',
-      teacherId: 1
-    };
+    // 通过TestBed.inject获取到当前模块可供使用的httpClient
+    // 前面我们通过这个方法获取过XAuthToken拦截器，道理是一样的
+    // 凡是可以被 自动注入 获取的，可以使用该方法来获取到其实例
     const httpClient = TestBed.inject(HttpClient) as HttpClient;
 
+    // 先使用zhangsan来登录一下系统
     const authString = 'zhangsan:codedemo.club';
     const authToken = btoa(authString);
     const httpHeaders = new HttpHeaders().append('Authorization', 'Basic ' + authToken);
     httpClient.get('http://angular.api.codedemo.club:81/teacher/login', {headers: httpHeaders})
       .subscribe(() => {
+        // 登录成功后，再新建班级，然后就不报401未认证的异常了
+        component.clazz = {
+          name: 'test',
+          teacherId: 1
+        };
         component.onSubmit();
+      }, error => {
+        console.log('登录发生异常，这极有可能是其它学员编辑了张三的用户名所导致，请尝试使用其它用户名');
       });
   });
 });
