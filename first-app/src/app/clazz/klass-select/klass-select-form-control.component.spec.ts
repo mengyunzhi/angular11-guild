1da045edf816f0e8432c5a6454a53fc1def2bb6b
panjie <3792535@qq.com>
2021-04-06 15:23:59 +0800

    temp ;

---
+++ first-app/src/app/clazz/klass-select/klass-select-form-control.component.spec.ts
@@ -0,0 +1,66 @@
+import {KlassSelectComponent} from './klass-select.component';
+import {TestBed} from '@angular/core/testing';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MockApiTestingInterceptor} from '@yunzhi/ng-mock-api/testing';
+import {TeacherMockApi} from '../../mock-api/teacher.mock.api';
+import {Component, OnInit} from '@angular/core';
+import {getTestScheduler} from 'jasmine-marbles';
+
+@Component({
+  template: '<h1 (click)="onTest()">Test:</h1><app-klass-select [formControl]="teacherIdFormControl"></app-klass-select>'
+})
+class TestComponent implements OnInit {
+  teacherIdFormControl = new FormControl(1);
+
+  ngOnInit(): void {
+    console.log('父组件初始化');
+  }
+
+  onTest(): void {
+    console.log('teacherId值为', this.teacherIdFormControl.value);
+  }
+}
+
+describe('KlassSelectComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [KlassSelectComponent, TestComponent],
+      imports: [
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule
+      ],
+      providers: [
+        {
+          provide: HTTP_INTERCEPTORS, multi: true,
+          useClass: MockApiTestingInterceptor.forRoot([
+            TeacherMockApi
+          ])
+        }
+      ]
+    })
+      .compileComponents();
+  });
+
+  fit('响应式表单', () => {
+    // 创建一个组件夹具（容器），这就像我们要测试显卡是否正常功能时，需要有一台供显卡工作的电脑一样。
+    // testFixture便是TestComponent这块显卡赖以工作的电脑
+    console.log('开始创建父组件');
+    const fixture = TestBed.createComponent(TestComponent);
+
+    // 获取testFixture这台电脑上的testComponent显卡
+    const component = fixture.componentInstance;
+    // 调用detectChanges()渲染V层，开始渲染V层中的子组件。
+    // 由于当前Test组件未请求后台，所以省略了getTestScheduler().flush();
+    // 当然了，写上也无防
+    console.log('首次渲染组件');
+    fixture.detectChanges();
+
+    // 模拟返回数据后，进行变更检测重新渲染子组件V层
+    console.log('触发后台模拟数据发送');
+    getTestScheduler().flush();
+    console.log('重新渲染组件');
+    fixture.detectChanges();
+  });
+});
--- first-app/src/app/clazz/klass-select/klass-select.component.ts
+++ first-app/src/app/clazz/klass-select/klass-select.component.ts
@@ -1,15 +1,23 @@
-import {Component, OnInit, EventEmitter, Output, Input} from '@angular/core';
+import {Component, OnInit, EventEmitter, Output, Input, forwardRef} from '@angular/core';
 import {Teacher} from '../../entity/teacher';
 import {HttpClient} from '@angular/common/http';
-import {FormControl} from '@angular/forms';
-
+import {ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
   selector: 'app-klass-select',
   templateUrl: './klass-select.component.html',
-  styleUrls: ['./klass-select.component.css']
+  styleUrls: ['./klass-select.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR, multi: true,
+      useExisting: forwardRef(() => {
+        console.log('useExisting->forwardRef中的回调方法被调用一次');
+        return KlassSelectComponent;
+      })
+    }
+  ]
 })
-export class KlassSelectComponent implements OnInit {
+export class KlassSelectComponent implements OnInit, ControlValueAccessor {
   teachers = new Array<Teacher>();
   teacherId = new FormControl();
 
@@ -25,13 +33,43 @@ export class KlassSelectComponent implements OnInit {
   constructor(private httpClient: HttpClient) {
   }
 
+  /**
+   * 将FormControl中的值通过此方法写入
+   * FormControl的值每变换一次，该方法将被重新执行一次
+   * 相当于@Input() set xxx
+   * @param obj 此类型取决于当前组件的接收类型，比如此时我们接收一个类型为number的teacherId
+   */
+  writeValue(obj: number): void {
+    console.log('writeValue is called');
+    this.teacherId.setValue(obj);
+  }
+
+  /**
+   * 组件需要向父组件弹值时，直接调用参数中的fn方法
+   * 相当于@Output()
+   * @param fn 此类型取决于当前组件的弹出值类型，比如我们当前将弹出一个类型为number的teacherId
+   */
+  registerOnChange(fn: (data: number) => void): void {
+    console.log(`registerOnChange is called`);
+    this.teacherId.valueChanges
+      .subscribe(data => fn(data));
+  }
+
+  registerOnTouched(fn: any): void {
+    console.warn('registerOnTouched not implemented');
+  }
+
   ngOnInit(): void {
+    console.log('教师选择组件初始化');
     // 关注teacherId
     this.teacherId.valueChanges
       .subscribe((data: number) => this.beChange.emit(data));
     // 获取所有教师
     this.httpClient.get<Array<Teacher>>('teacher')
       .subscribe(
-        teachers => this.teachers = teachers);
+        teachers => {
+          this.teachers = teachers;
+          console.log('教师选择组件接收到了数据');
+        });
   }
 }
