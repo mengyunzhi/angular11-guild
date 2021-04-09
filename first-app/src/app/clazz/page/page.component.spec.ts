d2eaa75a93670153589514f32fda9c5f873c86de
panjie <3792535@qq.com>
2021-04-09 15:09:45 +0800

    page

--- first-app/src/app/clazz/page/page.component.spec.ts
+++ first-app/src/app/clazz/page/page.component.spec.ts
@@ -2,6 +2,7 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {PageComponent} from './page.component';
 import {Page} from '../../entity/page';
+import {By} from '@angular/platform-browser';
 
 describe('PageComponent', () => {
   let component: PageComponent;
@@ -29,6 +30,42 @@ describe('PageComponent', () => {
       numberOfElements: 30
     });
     fixture.autoDetectChanges();
+  });
+
+  it('0页', () => {
+    component.page = new Page<any>({
+      content: [],
+      number: 0,
+      size: 20,
+      numberOfElements: 0
+    });
+    fixture.detectChanges();
+    const navHtml = fixture.debugElement.query(By.css('nav')).nativeElement as HTMLElement;
+    expect(navHtml.style.visibility).toEqual('hidden');
+  });
+
+  it('1页', () => {
+    component.page = new Page<any>({
+      content: [],
+      number: 0,
+      size: 20,
+      numberOfElements: 10
+    });
+    fixture.detectChanges();
+    const navHtml = fixture.debugElement.query(By.css('nav')).nativeElement as HTMLElement;
+    expect(navHtml.style.visibility).toEqual('hidden');
+  });
+
+  it('2页', () => {
+    component.page = new Page<any>({
+      content: [],
+      number: 0,
+      size: 20,
+      numberOfElements: 25
+    });
+    fixture.detectChanges();
+    const navHtml = fixture.debugElement.query(By.css('nav')).nativeElement as HTMLElement;
+    expect(navHtml.style.visibility).toEqual('visible');
   });
 
 });
