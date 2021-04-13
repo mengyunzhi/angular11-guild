4c7f378603a04744d12f23419aa8e71258eee6e7
panjie <3792535@qq.com>
2021-04-13 15:04:25 +0800

    step7.2.3

--- first-app/src/app/student/add/add.component.ts
+++ first-app/src/app/student/add/add.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {YzValidators} from '../../yz-validators';
 import {YzAsyncValidators} from '../../yz-async-validators';
-import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-add',
@@ -10,15 +9,17 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./add.component.css']
 })
 export class AddComponent implements OnInit {
-  formGroup = new FormGroup({
-    name: new FormControl('', Validators.required),
-    number: new FormControl('', Validators.required),
-    phone: new FormControl('', YzValidators.phone),
-    email: new FormControl(),
-    clazzId: new FormControl(null, Validators.required)
-  });
+  formGroup: FormGroup;
 
   constructor() {
+    const yzAsyncValidators = null as unknown as YzAsyncValidators;
+    this.formGroup = new FormGroup({
+      name: new FormControl('', Validators.required),
+      number: new FormControl('', Validators.required, yzAsyncValidators.numberNotExist()),
+      phone: new FormControl('', YzValidators.phone),
+      email: new FormControl(),
+      clazzId: new FormControl(null, Validators.required)
+    });
   }
 
   ngOnInit(): void {
