aa22021f1f341a116bc7f8263f2e32e484f1035c
panjie <3792535@qq.com>
2021-04-13 15:18:09 +0800

    7.2.3done

--- first-app/src/app/yz-async-validators.spec.ts
+++ first-app/src/app/yz-async-validators.spec.ts
@@ -1,7 +1,16 @@
 import { YzAsyncValidators } from './yz-async-validators';
+import {TestBed} from '@angular/core/testing';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 describe('YzAsyncValidators', () => {
-  it('should create an instance', () => {
-    expect(new YzAsyncValidators()).toBeTruthy();
+  it('should create an instance', async () => {
+    // 配置动态测试模块
+    await TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
+    // 获取动态测试模块中可被注入的HttpClient实例
+    const httpClient = TestBed.inject(HttpClient);
+
+    expect(new YzAsyncValidators(httpClient)).toBeTruthy();
   });
 });
