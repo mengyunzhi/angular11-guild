b5f63eb1e19cf85f231cc1d700b8d8170d19e63b
panjie <3792535@qq.com>
2021-06-07 10:51:56 +0800

    7.4.5

--- first-app/src/app/clazz/page/page.component.spec.ts
+++ first-app/src/app/clazz/page/page.component.spec.ts
@@ -68,4 +68,152 @@ describe('PageComponent', () => {
     expect(navHtml.style.visibility).toEqual('visible');
   });
 
+  it('将总页码的最大数量控制在7页', () => {
+    component.page = {
+      number: 2,
+      size: 20,
+      totalPages: 20
+    } as Page<any>;
+    fixture.autoDetectChanges();
+  });
+
+  it('总页码小于7', () => {
+    // 共5页，当前第4页
+    component.page = {
+      number: 3,
+      size: 20,
+      totalPages: 5
+    } as Page<any>;
+    fixture.detectChanges();
+    expect(component.pages.length).toBe(5);
+
+    // 共3页，当前第2页
+    component.page = {
+      number: 1,
+      size: 20,
+      totalPages: 3
+    } as Page<any>;
+    fixture.detectChanges();
+    expect(component.pages.length).toBe(3);
+  });
+
+  it('总页码等于7', () => {
+    // 共7页，当前第5页
+    component.page = {
+      number: 4,
+      size: 20,
+      totalPages: 7
+    } as Page<any>;
+    fixture.detectChanges();
+    expect(component.pages.length).toBe(7);
+
+    // 共7页，当前第7页
+    component.page = {
+      number: 6,
+      size: 20,
+      totalPages: 7
+    } as Page<any>;
+    fixture.detectChanges();
+    expect(component.pages.length).toBe(7);
+  });
+
+  it('总页数大于7, 第1页，共8页', () => {
+    // 共1页，当前第8页
+    component.page = {
+      number: 0,
+      size: 20,
+      totalPages: 8
+    } as Page<any>;
+    fixture.detectChanges();
+    // 共显示7页
+    expect(component.pages.length).toBe(7);
+    // 头是第1页
+    expect(component.pages[0]).toBe(0);
+    // 尾是第7页
+    expect(component.pages.pop()).toBe(6);
+  });
+
+  it('1 2 [3] 4 5 6 7', () => {
+    // 共3页，当前第8页
+    component.page = {
+      number: 2,
+      size: 20,
+      totalPages: 8
+    } as Page<any>;
+    fixture.detectChanges();
+    // 共显示7页
+    expect(component.pages.length).toBe(7);
+    // 头是第1页
+    expect(component.pages[0]).toBe(0);
+    // 尾是第7页
+    expect(component.pages.pop()).toBe(6);
+  });
+
+  it('1 2 3 [4] 5 6 7', () => {
+    // 共4页，当前第8页
+    component.page = {
+      number: 3,
+      size: 20,
+      totalPages: 8
+    } as Page<any>;
+    fixture.detectChanges();
+    // 共显示7页
+    expect(component.pages.length).toBe(7);
+    // 头是第1页
+    expect(component.pages[0]).toBe(0);
+    // 尾是第7页
+    expect(component.pages.pop()).toBe(6);
+  });
+
+  it('7 8 9 [10] 11 12 13', () => {
+    component.page = {
+      number: 9,
+      size: 20,
+      totalPages: 18
+    } as Page<any>;
+    fixture.detectChanges();
+    // 共显示7页
+    expect(component.pages.length).toBe(7);
+    expect(component.pages[0]).toBe(6);
+    expect(component.pages.pop()).toBe(12);
+  });
+
+  it('12 13 14 [15] 16 17 18', () => {
+    component.page = {
+      number: 14,
+      size: 20,
+      totalPages: 18
+    } as Page<any>;
+    fixture.detectChanges();
+    // 共显示7页
+    expect(component.pages.length).toBe(7);
+    expect(component.pages[0]).toBe(11);
+    expect(component.pages.pop()).toBe(17);
+  });
+
+  it('12 13 14 15 [16] 17 18', () => {
+    component.page = {
+      number: 15,
+      size: 20,
+      totalPages: 18
+    } as Page<any>;
+    fixture.detectChanges();
+    // 共显示7页
+    expect(component.pages.length).toBe(7);
+    expect(component.pages[0]).toBe(11);
+    expect(component.pages.pop()).toBe(17);
+  });
+
+  it('12 13 14 15 16 17 [18]', () => {
+    component.page = {
+      number: 17,
+      size: 20,
+      totalPages: 18
+    } as Page<any>;
+    fixture.detectChanges();
+    // 共显示7页
+    expect(component.pages.length).toBe(7);
+    expect(component.pages[0]).toBe(11);
+    expect(component.pages.pop()).toBe(17);
+  });
 });
--- first-app/src/app/clazz/page/page.component.ts
+++ first-app/src/app/clazz/page/page.component.ts
@@ -22,10 +22,33 @@ export class PageComponent implements OnInit {
     console.log('set page被调用');
     console.log('当前页', this.inputPage.number);
     console.log('总页数', this.inputPage.totalPages);
+    // 初始化最大页码，起始页码
+    let maxCount;
+    let begin;
+
+    if (this.inputPage.totalPages > 7) {
+      // 大于7页时，仅显示7页
+      maxCount = 7;
+
+      // 起始页为当前页-3.比如当前页为10，则应该由7页开始
+      begin = this.inputPage.number - 3;
+      if (begin < 0) {
+        // 判断是否越界，可以删除下一行代码查看错误的效果
+        begin = 0;
+      } else if (begin > this.inputPage.totalPages - 7) {
+        // 判断是否越界，可以删除下一行代码查看错误的效果
+        begin = this.inputPage.totalPages - 7;
+      }
+    } else {
+      // 小于等于7页时，使用原算法。页码数为总页数，页码由0开始
+      maxCount = this.inputPage.totalPages;
+      begin = 0;
+    }
+
     // 生成页数数组
     this.pages = [];
-    for (let i = 0; i < this.inputPage.totalPages; i++) {
-      this.pages.push(i);
+    for (let i = 0; i < maxCount; i++, begin++) {
+      this.pages.push(begin);
     }
     // 设置当前页
     this.currentPage = this.inputPage.number;
